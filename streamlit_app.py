d462f970c020f03e3cfedd344459f8579d295b26
eeearth123 <earth1732@gmail.com>
2025-09-11 15:26:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -571,10 +571,8 @@ if st.button("Predict Price (ล้านบาท)"):
                 num_cols = _resolve_num_cols(X_train_all)
                 x_num = _prep_num(X[num_cols], num_cols)
                 x_scaled = _robust_scale_transform_nums(x_num, r_scaler, s_scaler)
-                S_num = _rbf_numeric_similarity(
-                    x_scaled.values[0], Xt_scaled_train,
-                    k=CONF_PARAMS["k_num"], tau=CONF_PARAMS["tau"]
-                )
+                S_num = _rbf_numeric_similarity(x_scaled.values[0], Xt_scaled_train,k=CONF_PARAMS["k_num"], tau=CONF_PARAMS["tau"])
+
 
                 # Categorical similarity (+ unseen penalties)
                 S_cat, unseen_frac = _categorical_similarity(
@@ -613,3 +611,4 @@ if st.button("Predict Price (ล้านบาท)"):
 
     except Exception as e:
         st.error(f"ทำนายไม่สำเร็จ: {e}")
+
